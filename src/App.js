e5b0178b564eaa07e06a63dea0bac99d61925ce6
Ben Wright <benwright3103@gmail.com>
2017-08-11 19:28:32 -0400

    added intitial files

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import Header from './Header';
+import Footer from './Footer';
+import Container from './Container';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div >
+        <Navbar />
+        <Header />
+        <Container />
+        <Footer  />
+
+
+
+
+
+
+
+
+
+        <hr />
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
